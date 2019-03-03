1513d3aa4b796101d1c9ee20467aa4c6e9494fd1
Linton Ye <linton@jimulabs.com>
2019-03-03 12:46:39 -0800

    Add Typography

---
+++ rmwc.framerfx/code/Typography.tsx
@@ -0,0 +1,46 @@
+import * as React from "react";
+import { PropertyControls, ControlType } from "framer";
+import "@material/typography/dist/mdc.typography.css";
+import { Typography as _Typography, TypographyT } from "@rmwc/typography";
+import FramerXWrapper from "./FramerXWrapper";
+
+type Props = { text: string; variant: TypographyT };
+
+export class Typography extends React.Component<Props> {
+  render() {
+    const { text, variant } = this.props;
+    return (
+      <FramerXWrapper>
+        <_Typography use={variant}>{text}</_Typography>
+      </FramerXWrapper>
+    );
+  }
+
+  static defaultProps: Props = {
+    variant: "body1",
+    text: "Hello world"
+  };
+
+  static propertyControls: PropertyControls<Props> = {
+    text: { type: ControlType.String, title: "Text" },
+    variant: {
+      type: ControlType.Enum,
+      title: "variant",
+      options: [
+        "headline1",
+        "headline2",
+        "headline3",
+        "headline4",
+        "headline5",
+        "headline6",
+        "subtitle1",
+        "subtitle2",
+        "body1",
+        "body2",
+        "caption",
+        "button",
+        "overline"
+      ]
+    }
+  };
+}
