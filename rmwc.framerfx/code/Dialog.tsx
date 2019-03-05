a50e440f42ceec50aeb383ec3e35fb0a2b48cec5
Linton Ye <linton@jimulabs.com>
2019-03-05 10:31:18 -0800

    Fix dialog position

--- rmwc.framerfx/code/Dialog.tsx
+++ rmwc.framerfx/code/Dialog.tsx
@@ -11,45 +11,81 @@ import "@material/button/dist/mdc.button.css";
 import { iconPropertyControls, processIconProps } from "./framerx-integration";
 import { isCanvas, cloneFrameless, makeFrameRelative } from "./framerx-utils";
 
-export function Dialog(props) {
-  const { title, content, actions, width, height, ...rest } = processIconProps(
-    props
-  );
-  return isCanvas() ? (
-    <div
-      style={{
-        backgroundColor: "rgba(100,200,200,0.3)",
-        pointerEvents: "none"
-      }}
-    >
-      Dialog: {title}
-    </div>
-  ) : (
-    <_Dialog {...rest}>
-      <DialogTitle>{title}</DialogTitle>
-      <DialogContent>{cloneFrameless(content)}</DialogContent>
-      <DialogActions>{cloneFrameless(actions)}</DialogActions>
-    </_Dialog>
-  );
-}
+export class Dialog extends React.Component {
+  render() {
+    const {
+      title,
+      content,
+      actions,
+      width,
+      height,
+      ...rest
+    } = processIconProps(this.props);
 
-const propertyControls: PropertyControls = {
-  title: {
-    type: ControlType.String,
-    title: "Title"
-  },
-  content: {
-    type: ControlType.ComponentInstance,
-    title: "Content"
-  },
-  actions: {
-    type: ControlType.ComponentInstance,
-    title: "Actions"
+    return isCanvas() ? (
+      <div
+        style={{
+          backgroundColor: "rgba(100,200,200,0.3)",
+          height: "100%"
+        }}
+      >
+        Dialog: {title}
+        {cloneFrameless(content)}
+        {cloneFrameless(actions)}
+      </div>
+    ) : (
+      <_Dialog {...rest} ref={r => (this.dialog = r)}>
+        <DialogTitle>{title}</DialogTitle>
+        <DialogContent>{cloneFrameless(content)}</DialogContent>
+        <DialogActions>{cloneFrameless(actions)}</DialogActions>
+      </_Dialog>
+    );
   }
-};
 
-Dialog.propertyControls = propertyControls;
+  findParentFrameDiv = () => {
+    if (this.dialog) {
+      this.frameDiv = this.dialog.container.parentNode.parentNode;
+    }
+  };
 
-Dialog.defaultProps = {
-  open: false
-};
+  // This is a hack to make the dialog and its scrim to
+  // go outside of the parent div that Framer X adds to
+  // every component instance.
+  updateParentFrameDiv = ({ open, width, height }) => {
+    if (!isCanvas()) {
+      this.frameDiv.style = `display: ${
+        open ? "block" : "none"
+      }; height: 100vh;`;
+    }
+  };
+
+  componentDidMount = () => {
+    this.findParentFrameDiv();
+    this.updateParentFrameDiv(this.props);
+  };
+
+  componentDidUpdate = (prevProps, prevState) => {
+    if (prevProps.open !== this.props.open) {
+      this.updateParentFrameDiv(this.props);
+    }
+  };
+
+  static propertyControls: PropertyControls = {
+    title: {
+      type: ControlType.String,
+      title: "Title"
+    },
+    content: {
+      type: ControlType.ComponentInstance,
+      title: "Content"
+    },
+    actions: {
+      type: ControlType.ComponentInstance,
+      title: "Actions"
+    }
+  };
+
+  static defaultProps = {
+    open: false
+  };
+}
