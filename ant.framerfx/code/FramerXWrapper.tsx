1be1b602846018d3d7e753a1986e1828ae26bdc3
Linton Ye <linton@jimulabs.com>
2019-03-10 14:42:59 -0700

    Load less from client

--- ant.framerfx/code/FramerXWrapper.tsx
+++ ant.framerfx/code/FramerXWrapper.tsx
@@ -1,18 +1,39 @@
 import * as React from "react";
-import antd from "antd/dist/antd.less";
 // import "./styles.less";
 import Helmet from "react-helmet";
 import { url } from "framer/resource";
 
-function Wrapper({ children }) {
-  return (
-    <>
-      <Helmet>
-        <link rel="stylesheet" type="text/css" href={antd} />
-      </Helmet>
-      {children}
-    </>
-  );
+class FramerXWrapper extends React.Component {
+  //// Below does NOT work!
+  // componentDidMount = () => {
+  //   try {
+  //     window.less.modifyVars({ "@primary-color": "red" });
+  //   } catch (e) {
+  //     console.error(e);
+  //   }
+  // };
+
+  render() {
+    const { children } = this.props;
+    return (
+      <>
+        <Helmet>
+          <link
+            rel="stylesheet/less"
+            type="text/css"
+            href={url("node_modules/antd/dist/antd.less")}
+          />
+          <script>{`less = { javascriptEnabled: true, async: false, fileAsync: false }`}</script>
+          <script
+            src={url("node_modules/less/dist/less.js")}
+            data-env="development"
+          />
+          <script>{`setTimeout(()=>less.modifyVars({ "@primary-color": "orange" }), 500)`}</script>
+        </Helmet>
+        {children}
+      </>
+    );
+  }
 }
 
-export const FramerXWrapper = Wrapper;
+export { FramerXWrapper };
