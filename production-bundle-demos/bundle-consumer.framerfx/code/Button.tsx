043a45d20153c4c344868dd6d4c7140954655c62
Linton Ye <linton@jimulabs.com>
2019-03-13 16:36:00 -0700

    Make the button primary

--- production-bundle-demos/bundle-consumer.framerfx/code/Button.tsx
+++ production-bundle-demos/bundle-consumer.framerfx/code/Button.tsx
@@ -2,5 +2,5 @@ import * as React from "react";
 import { Button as _Button } from "../lib/main";
 
 export function Button() {
-  return <_Button>Hello!</_Button>;
+  return <_Button type="primary">Hello!</_Button>;
 }
