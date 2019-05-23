a12f427791d3916f848907d667dc4856764b1abd
Linton Ye <linton@jimulabs.com>
2019-05-22 23:14:50 -0700

    Read json for list items

--- rmwc.framerfx/code/List.tsx
+++ rmwc.framerfx/code/List.tsx
@@ -1,16 +1,36 @@
 import * as React from "react"
+import { useState, useEffect } from "react"
 import "@material/list/dist/mdc.list.css"
 import "@rmwc/list/collapsible-list.css"
-import { List as _List, CollapsibleList } from "@rmwc/list"
+import { List as _List, CollapsibleList, SimpleListItem } from "@rmwc/list"
 import { cloneFrameless } from "@framer/lintonye.learnreactdesign-ds/code/tools/framerx-utils"
 import FramerXWrapper from "./FramerXWrapper"
 import { addPropertyControls, ControlType } from "framer"
 
-export function List({ children, collapsible, ...rest }) {
-  const listItems = cloneFrameless(children)
+async function readItemsFromJson(jsonUrl) {
+  const jsonResponse = await fetch(jsonUrl)
+  const items = await jsonResponse.json()
+
+  return items.map(({ title, graphic }, idx) => (
+    <SimpleListItem text={title} graphic={graphic} key={idx} />
+  ))
+}
+
+export function List({ children, collapsible, itemsJsonUrl, ...rest }) {
+  const [listItems, setListItems] = useState(cloneFrameless(children))
   const [firstItem, ...restItems] = listItems
+
   const ListComp = collapsible ? CollapsibleList : _List
   const props = collapsible ? { handle: firstItem, ...rest } : rest
+  useEffect(() => {
+    if (itemsJsonUrl) {
+      async function loadJson() {
+        const items = await readItemsFromJson(itemsJsonUrl)
+        setListItems(items)
+      }
+      loadJson()
+    }
+  }, [itemsJsonUrl])
   return (
     <FramerXWrapper>
       <ListComp {...props}>{collapsible ? restItems : listItems}</ListComp>
@@ -22,5 +42,9 @@ addPropertyControls(List, {
   collapsible: {
     type: ControlType.Boolean,
     defaultValue: false
+  },
+  itemsJsonUrl: {
+    type: ControlType.File,
+    allowedFileTypes: ["json"]
   }
 })
